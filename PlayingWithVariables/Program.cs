5336604e37504e663783a19b5a97795424f53363
Ernest Acosta <ernestacosta@icloud.com>
2022-06-01 15:40:20 -0700

    initial commit

---
+++ PlayingWithVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PlayingWithVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dogName = "Ralph";
+            int dogAge = 10;
+            char firstInitial = 'R';
+            bool lovesHiking = true;
+            double tennisBalls = 32;
+            decimal weight = 70.3m;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is " +
+                $"{dogAge} years old and has {tennisBalls} tennis balls " +
+                $"and it is {lovesHiking} that he loves to hike, which keeps " +
+                $"him at a healthy weight of {weight} lbs"); 
+
+        }
+    }
+}
